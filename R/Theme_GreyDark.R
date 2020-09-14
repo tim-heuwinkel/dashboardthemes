41d0180326bbca7a5e5d7ecc4a3dece75dd3309e
tim-heuwinkel <65255100+tim-heuwinkel@users.noreply.github.com>
2020-09-14 19:35:41 +0200

    Update Theme_GreyDark.R

--- R/Theme_GreyDark.R
+++ R/Theme_GreyDark.R
@@ -35,7 +35,7 @@ logo_grey_dark <- function(boldText = "Shiny", mainText = "App", badgeText = "v1
 #' @seealso \code{\link{shinyDashboardThemeDIY}}
 #' @export
 theme_grey_dark <- shinyDashboardThemeDIY(
-
+  
   ### general
   appFontFamily = "Arial"
   ,appFontColor = "rgb(205,205,205)"
@@ -45,53 +45,53 @@ theme_grey_dark <- shinyDashboardThemeDIY(
   ,warningFontColor = "rgb(255,255,255)"
   ,dangerFontColor = "rgb(255,255,255)"
   ,bodyBackColor = "rgb(45,55,65)"
-
+  
   ### header
   ,logoBackColor = "rgb(70,80,90)"
-
+  
   ,headerButtonBackColor = "rgb(70,80,90)"
   ,headerButtonIconColor = "rgb(25,35,45)"
   ,headerButtonBackColorHover = "rgb(40,50,60)"
   ,headerButtonIconColorHover = "rgb(0,0,0)"
-
+  
   ,headerBackColor = "rgb(70,80,90)"
   ,headerBoxShadowColor = ""
   ,headerBoxShadowSize = "0px 0px 0px"
-
+  
   ### sidebar
   ,sidebarBackColor = "rgb(52,62,72)"
   ,sidebarPadding = 0
-
+  
   ,sidebarMenuBackColor = "transparent"
   ,sidebarMenuPadding = 0
   ,sidebarMenuBorderRadius = 0
-
+  
   ,sidebarShadowRadius = ""
   ,sidebarShadowColor = "0px 0px 0px"
-
+  
   ,sidebarUserTextColor = "rgb(205,205,205)"
-
+  
   ,sidebarSearchBackColor = "rgb(45,55,65)"
   ,sidebarSearchIconColor = "rgb(153,153,153)"
   ,sidebarSearchBorderColor = "rgb(45,55,65)"
-
+  
   ,sidebarTabTextColor = "rgb(205,205,205)"
   ,sidebarTabTextSize = 14
   ,sidebarTabBorderStyle = "none"
   ,sidebarTabBorderColor = "none"
   ,sidebarTabBorderWidth = 0
-
+  
   ,sidebarTabBackColorSelected = "rgb(70,80,90)"
   ,sidebarTabTextColorSelected = "rgb(255,255,255)"
   ,sidebarTabRadiusSelected = "5px"
-
+  
   ,sidebarTabBackColorHover = "rgb(55,65,75)"
   ,sidebarTabTextColorHover = "rgb(255,255,255)"
   ,sidebarTabBorderStyleHover = "none"
   ,sidebarTabBorderColorHover = "none"
   ,sidebarTabBorderWidthHover = 0
   ,sidebarTabRadiusHover = "5px"
-
+  
   ### boxes
   ,boxBackColor = "rgb(52,62,72)"
   ,boxBorderRadius = 5
@@ -104,7 +104,7 @@ theme_grey_dark <- shinyDashboardThemeDIY(
   ,boxSuccessColor = "rgb(155,240,80)"
   ,boxWarningColor = "rgb(240,80,210)"
   ,boxDangerColor = "rgb(240,80,80)"
-
+  
   ,tabBoxTabColor = "rgb(52,62,72)"
   ,tabBoxTabTextSize = 14
   ,tabBoxTabTextColor = "rgb(205,205,205)"
@@ -112,29 +112,29 @@ theme_grey_dark <- shinyDashboardThemeDIY(
   ,tabBoxBackColor = "rgb(52,62,72)"
   ,tabBoxHighlightColor = "rgb(70,80,90)"
   ,tabBoxBorderRadius = 5
-
+  
   ### inputs
   ,buttonBackColor = "rgb(230,230,230)"
   ,buttonTextColor = "rgb(0,0,0)"
   ,buttonBorderColor = "rgb(50,50,50)"
   ,buttonBorderRadius = 5
-
+  
   ,buttonBackColorHover = "rgb(180,180,180)"
   ,buttonTextColorHover = "rgb(50,50,50)"
   ,buttonBorderColorHover = "rgb(50,50,50)"
-
-  ,textboxBackColor = "rgb(68,80,90)"
+  
+  ,textboxBackColor = "rgb(130,130,130)"
   ,textboxBorderColor = "rgb(76,90,103)"
   ,textboxBorderRadius = 5
-  ,textboxBackColorSelect = "rgb(80,90,100)"
+  ,textboxBackColorSelect = "rgb(0,0,0)"
   ,textboxBorderColorSelect = "rgb(255,255,255)"
-
+  
   ### tables
   ,tableBackColor = "rgb(52,62,72)"
   ,tableBorderColor = "rgb(70,80,90)"
   ,tableBorderTopSize = 1
   ,tableBorderRowSize = 1
-
+  
 )
 
 # End Exclude Linting
